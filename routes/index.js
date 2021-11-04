b699784fe3e1b7359fc62c5f692c381b205c3af9
François Cordier <francoiscordier.ece@gmail.com>
2021-11-04 11:46:08 +0100

    sign-up route

--- routes/index.js
+++ routes/index.js
@@ -26,18 +26,42 @@ router.get("/", function (req, res, next) {
   res.render("index");
 });
 
-router.post("/sign-up", async function(req, res){
+router.post("/sign-up", async function (req, res) {
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const email = req.body.email;
+  const password = req.body.password;
+  const alreadyRegistered = await userModel.findOne({ email: email });
 
+  if (!alreadyRegistered) {
+    const newUser = new userModel({
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      password: password,
+    });
+
+    const userSaved = await newUser.save();
+
+    req.session.userId = userSaved._id;
+
+    res.redirect("/homepage");
+  } else {
+    res.redirect("/");
+  }
+});
+
+router.post("/sign-in", async function (req, res) {
   res.redirect("/homepage");
 });
 
-router.post("/sign-in", async function(req, res){
-  
-  res.redirect("/homepage");
-});
-
-router.get("/homepage", async function(req, res){
-  res.render("homepage");
+router.get("/homepage", async function (req, res) {
+  if (req.session.userId) {
+    console.log(req.session);
+    res.render("homepage");
+  } else {
+    res.redirect("/");
+  }
 });
 
 module.exports = router;
