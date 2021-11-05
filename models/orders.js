9d0ce3639c31f63028c2a4c738df0899042040a7
François Cordier <francoiscordier.ece@gmail.com>
2021-11-05 17:53:34 +0100

    express validator signup form

--- models/orders.js
+++ models/orders.js
@@ -1,9 +1,8 @@
 const mongoose = require("mongoose");
 
 const orderSchema = mongoose.Schema({
-    journey: [ {type: mongoose.Schema.Types.ObjectId, ref: "journeys"}],
-    user: [ {type: mongoose.Schema.Types.ObjectId, ref: "users"}],
-   
+  journey: [{ type: mongoose.Schema.Types.ObjectId, ref: "journeys" }],
+  user: [{ type: mongoose.Schema.Types.ObjectId, ref: "users" }],
 });
 
 const orderModel = mongoose.model("orders", orderSchema);
--- routes/index.js
+++ routes/index.js
@@ -3,54 +3,103 @@ const router = express.Router();
 const journeyModel = require("../models/journeys");
 const userModel = require("../models/users");
 const orderModel = require("../models/orders");
+const { body, validationResult } = require("express-validator");
+const { check } = require("express-validator");
 
-router.get("/", function (req, res, next) {
+router.get("/", function (req, res) {
+  const errors = validationResult(req).errors;
+
+  req.session.form = req.session.form
+    ? req.session.form
+    : { firstName: "", lastName: "", email: "" };
+
   if (req.session.userInfo) {
     console.log(req.session);
     res.redirect("/homepage");
   } else {
-    res.render("index", { session: req.session.userInfo });
+    res.render("index", {
+      session: req.session.userInfo,
+      errors,
+      sessionForm: req.session.form,
+    });
   }
   console.log(req.session);
 });
 
 router.get("/homepage", async function (req, res) {
   if (req.session.userInfo) {
-    console.log(req.session);
-    res.render("homepage", { session: req.session.userInfo });
+    const citiesList = await journeyModel.distinct("departure");
+    console.log(citiesList);
+    res.render("homepage", { session: req.session.userInfo, citiesList });
   } else {
     res.redirect("/");
   }
 });
 
-router.post("/sign-up", async function (req, res) {
-  const firstName = req.body.firstName;
-  const lastName = req.body.lastName;
-  const email = req.body.email;
-  const password = req.body.password;
-  const alreadyRegistered = await userModel.findOne({ email: email });
-
-  if (!alreadyRegistered) {
-    const newUser = new userModel({
-      firstName: firstName,
-      lastName: lastName,
-      email: email,
-      password: password,
+router.post(
+  "/sign-up",
+  body("firstName")
+    .not()
+    .isEmpty()
+    .trim()
+    .escape()
+    .withMessage("Please enter your first name"),
+  body("lastName")
+    .not()
+    .isEmpty()
+    .trim()
+    .escape()
+    .withMessage("Please enter your last name"),
+  body("email").custom(async (email) => {
+    return await userModel.findOne({ email: email }).then((user) => {
+      if (user) {
+        return Promise.reject("E-mail already in use");
+      }
     });
+  }),
+  check("password", "The password must be 5+ chars long and contain a number")
+    .not()
+    .isIn(["123", "password", "god"])
+    .withMessage("Do not use a common word as the password")
+    .isLength({ min: 5 })
+    .matches(/\d/),
+  check("email").isEmail().withMessage("Please enter a valid email address"),
+  async function (req, res) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      req.session.form.firstName = req.body.firstName;
+      req.session.form.lastName = req.body.lastName;
+      req.session.form.email = req.body.email;
+      console.log(req.session);
+      console.log(errors);
+      res.render("index", {
+        session: req.session.userInfo,
+        errors: errors.errors,
+        sessionForm: req.session.form,
+      });
+    } else {
+      const { firstName, lastName, email, password } = req.body;
+      // const alreadyRegistered = await userModel.findOne({ email: email });
 
-    const userSaved = await newUser.save();
+      const newUser = new userModel({
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        password: password,
+      });
 
-    req.session.userInfo = {
-      id: userSaved._id,
-      firstName: userSaved.firstName,
-      lastName: userSaved.lastName,
-    };
+      const userSaved = await newUser.save();
 
-    res.redirect("/homepage");
-  } else {
-    res.redirect("/");
+      req.session.userInfo = {
+        id: userSaved._id,
+        firstName: userSaved.firstName,
+        lastName: userSaved.lastName,
+      };
+
+      res.redirect("/homepage");
+    }
   }
-});
+);
 
 router.post("/sign-in", async function (req, res) {
   const email = req.body.email;
@@ -88,7 +137,7 @@ router.post("/search", async function (req, res) {
     arrival: to,
     date: date,
   });
-
+  console.log(results);
   res.render("searchresult", { results, session: req.session.userInfo, date });
 });
 
@@ -108,7 +157,7 @@ router.get("/add-to-cart", async function (req, res) {
 
 router.get("/cart", function (req, res) {
   const session = req.session;
-  if(!req.session.cart){
+  if (!req.session.cart) {
     req.session.cart = [];
   }
   const myCart = req.session.cart;
@@ -128,12 +177,12 @@ router.get("/checkout", async function (req, res) {
   const user = req.session.userInfo;
 
   for (let item of myCart) {
-    var newOrder = await orderModel({
+    const newOrder = await orderModel({
       journey: item._id,
       user: user.id,
     });
     const orderSaved = await newOrder.save();
-    req.session.cart=[];
+    req.session.cart = [];
   }
   res.redirect("/homepage");
 });
@@ -143,7 +192,7 @@ router.get("/my-journeys", async function (req, res) {
     .find({ user: req.session.userInfo.id })
     .populate("journey");
 
-  res.render("myjourneys", { myJourneys, session : req.session });
+  res.render("myjourneys", { myJourneys, session: req.session });
 });
 
 module.exports = router;
